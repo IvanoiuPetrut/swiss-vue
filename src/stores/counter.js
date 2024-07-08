835ac6c7f90caa3aea7c3ca209c2171039d29420
Petrut <ivanoiu.petrut27@gmail.com>
2024-07-08 16:48:51 +0200

    add store

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,8 +2,11 @@ import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
 export const useCounterStore = defineStore("counter", () => {
+
   const count = ref(0);
-  const doubleCount = computed(() => count.value * 2);
+
+  const doubleCount = computed(() => count.value * 2); //getters
+
   function increment() {
     count.value++;
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,12 @@
+<script setup>
+import { useCounterStore } from "../stores/counter.js";
+
+const counterStore = useCounterStore();
+</script>
+
 <template>
   <h1>This is the home page</h1>
+  <p>Count: {{ counterStore.count }}</p>
+  <p>Double count {{ counterStore.doubleCount }}</p>
+  <button @click="counterStore.increment()">Increment</button>
 </template>
